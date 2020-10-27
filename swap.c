cc2b08cc20d3f86d1964df439bf4ffcc876412f7
sumansmita-1 <73527177+sumansmita-1@users.noreply.github.com>
2020-10-27 15:49:27 +0530

    swapping two numbers without using third variable
    the result will be interchange the value between a and b.

---
+++ swap.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int a=10, b=20;
+	printf("Before swap a=%d and b=%d",a,b);
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	printf("\n After swap a=%d, b=%d",a,b);
+	return 0;	
+}
